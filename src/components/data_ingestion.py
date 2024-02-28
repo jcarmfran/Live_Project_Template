3df6f4eb4738b51b1e3703bed071ba65ab1b67b7
Francisco Carmona <86492879+jcarmfran@users.noreply.github.com>
2024-02-28 13:26:54 -0600

    Update data_ingestion.py
    adding comments

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+
+# to create class variables
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
@@ -12,20 +14,35 @@ from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
+
+# dataclass as decorator: able to directly define class variable
 @dataclass
 class DataIngestionConfig:
+    '''
+    Creating required inputs. 
+    Class variables housing stated filepaths to respective data injection component.
+    Data injection component output will save all the files in this path (e.g., train data, test data, etc.)
+    
+    artifacts: folder for outputs
+    '''
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
+    '''
+    
+    '''
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig() # train/test/raw_data_path will get saved within ingestion_config variable
 
     def initiate_data_ingestion(self):
+        '''
+        For freshers: creating function here as opposed to creating a client (e.g., MongoDB, SQL, etc.) in utils.py
+        '''
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv') # just reading from local file
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
